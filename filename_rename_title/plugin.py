2b6b26a55d3d48bd6478be494a49beb24ce4020d
Vincent Boulaye <vincent.boulaye@orange.com>
2021-07-19 23:24:28 +0200

    rename in nav

--- filename_rename_title/plugin.py
+++ filename_rename_title/plugin.py
@@ -1,20 +1,44 @@
 from mkdocs.plugins import BasePlugin
+from mkdocs.structure.nav import Navigation as MkDocsNavigation
 import re
+from mkdocs.structure.files import Files, File
+from mkdocs.config import config_options, Config
 
 
 class FilenameRenameTitlePlugin(BasePlugin):
-    def on_page_markdown(self, markdown, page, config, site_navigation=None, **kwargs):
-        """
-        The page_markdown event is called after the page's markdown is loaded from
-        file and can be used to alter the Markdown source text.
-        The meta- data has been stripped off and is available as page.meta at this point.
-        """
-        if 'title' in page.meta:
-            return markdown
-        if page.is_homepage:
-            return markdown
-        page.title = re.sub('^[0-9]{1,2}_','',page.file.name).replace('-', ' ').replace('_', ' ')
+    def on_nav(self, nav: MkDocsNavigation, config: Config, files: Files):
+
+        for item in nav.items:
+            self.process_item_title(item)
+        return nav
+
+    def process_item_title(self, nav: MkDocsNavigation):
+        if nav.title:
+            nav.title = self.format_title(nav.title)
+        if nav.items:
+            for item in nav.items:
+                self.process_item_title(item)
+        return nav
+
+    # def on_page_markdown(self, markdown, page, config, site_navigation=None, **kwargs):
+    #     """
+    #     The page_markdown event is called after the page's markdown is loaded from
+    #     file and can be used to alter the Markdown source text.
+    #     The meta- data has been stripped off and is available as page.meta at this point.
+    #     """
+    #     if 'title' in page.meta:
+    #         return markdown
+    #     if page.is_homepage:
+    #         return markdown
+    #     page.title = re.sub('^[0-9]{1,2}_','',page.file.name).replace('-', ' ').replace('_', ' ')
+    #     # Capitalize if the filename was all lowercase, otherwise leave it as-is.
+    #     if page.title.lower() == page.title:
+    #         page.title = page.title.capitalize()
+    #     return markdown
+
+    def format_title(self, title):
+        title = re.sub('^[0-9]{1,2}_', '', title).replace('-', ' ').replace('_', ' ')
         # Capitalize if the filename was all lowercase, otherwise leave it as-is.
-        if page.title.lower() == page.title:
-            page.title = page.title.capitalize()
-        return markdown
+        if title.lower() == title:
+            title = title.capitalize()
+        return title
